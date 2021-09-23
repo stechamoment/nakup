c86c5920fad532afa474413b1cd28a046f69adea
Libor Štecha <90755678+stechamoment@users.noreply.github.com>
2021-09-23 15:28:16 +0200

    Add files via upload

---
+++ untitled/src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String od = args[0];
+        int cena = 0;
+        int i = 0;
+        String[] pole = new String[args.length];
+        if (od.equals("1") || od.equals("2") || od.equals("3")) {
+            while (i < args.length) {
+                if (args[i].equals("1")) {
+                    pole[i] = "rohlik";
+                    cena = cena + 1;
+                }else if (args[i].equals("2")) {
+                    pole[i] = "mliko";
+                    cena = cena + 10;
+                }else if (args[i].equals("3")) {
+                    pole[i] = "cokolada";
+                    cena = cena + 30;
+                }
+                i++;
+            }{
+                i = 0;
+                System.out.println("kosik:");
+                while(i < args.length){
+                    System.out.println(pole[i]);
+                    i++;
+                }
+                System.out.println("cena: "+cena);
+            }
+        } else {
+            System.out.println("1 je rohlik");
+            System.out.println("2 je mliko");
+            System.out.println("3 je cokolada");
+        }
+
+    }
+}
